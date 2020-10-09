65c9af2d149da1d13c38a2870d1083d711eb9c3f
nettle94 <72432834+nettle94@users.noreply.github.com>
2020-10-09 12:50:29 +0900

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Class1
+    {
+        double wight;
+        double height;
+        string Color;
+        string Material;
+        string forma;
+
+        public Class1(double wight, double height, string color, string material, string forma)
+
+        {
+            this.wight = wight;
+            this.height = height;
+            Color = color;
+            Material = material;
+            this.forma = forma;
+        }
+
+        public string ReturnForm()
+        {
+            return forma;
+        }
+        string ReturnColorTable()
+        {
+            return Color;
+           
+        }  
+        
+        }
+    
+    }
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        Class1 stol = new Class1(30, 15, "красный", "Дерево", "Квадрат");
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.ReturnForm());
+            Console.ReadLine();
+        }
+    }
+}
